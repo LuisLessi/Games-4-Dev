f4c2b2e85398fac51b85857dced3d7a3d0bda284
luisadsfatec@hotmail.com <luisadsfatec@hotmail.com>
2023-06-19 01:14:59 -0300

    Conectado projeto com o DB e feito a função de chamar a tabela games

---
+++ application/models/games_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class Games_model extends CI_Model {
+
+    public function index()
+    {
+        "SELECT FROM tb_games";
+        
+        return $this->db->get("tb_games")->result_array();
+    }
+}
